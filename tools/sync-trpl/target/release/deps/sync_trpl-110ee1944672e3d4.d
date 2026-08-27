154999bec1108476b7ed6a86e56df91df2e33164
Joe Hsiao <joe.phsiao@gmail.com>
2026-08-27 16:00:49 -0500

    Remove the target folder that contains rust binary

--- tools/sync-trpl/target/release/deps/sync_trpl-110ee1944672e3d4.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/trpl-zh-tw/trpl-zh-tw/tools/sync-trpl/target/release/deps/sync_trpl-110ee1944672e3d4.d: src/main.rs
-
-/home/runner/work/trpl-zh-tw/trpl-zh-tw/tools/sync-trpl/target/release/deps/sync_trpl-110ee1944672e3d4: src/main.rs
-
-src/main.rs:
--- tools/sync-trpl/target/release/sync-trpl.d
+++
@@ -1,1 +0,0 @@
-/home/runner/work/trpl-zh-tw/trpl-zh-tw/tools/sync-trpl/target/release/sync-trpl: /home/runner/work/trpl-zh-tw/trpl-zh-tw/tools/sync-trpl/src/main.rs
